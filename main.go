e4d4a822794c0ac798382fd4866413f949c914bb
Gatovsky <101161255+hharieta@users.noreply.github.com>
2023-10-23 11:38:22 -0500

    condicionales

--- main.go
+++ main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/hharieta/godesde0/variables"
+	"runtime"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	variables.MostrarEnteros()
-	variables.OtrasVariables()
-	estado, texto := variables.Convert2Text(4)
+	// fmt.Println("Hello, World!")
+	// variables.MostrarEnteros()
+	// variables.OtrasVariables()
+	// estado, texto := variables.Convert2Text(4)
 
-	fmt.Println("Estado: ", estado, "Texto: ", texto)
+	// fmt.Println("Estado: ", estado, "Texto: ", texto)
+
+	if os := runtime.GOOS; os == "Linux" || os == "darwin" {
+		fmt.Println("Esto no es Windows: ", os, "god")
+	} else {
+		fmt.Println("Esto es un Windowszzz")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
